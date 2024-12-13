dc012522c9b2ff460eadfce257230621e261f46a
Oleh Martsokha <o.martsokha@gmail.com>
2024-12-13 12:01:11 +0100

    initial commit: ci nightly 4/n

--- crates/schema/constraints/mod.rs
+++ crates/schema/constraints/mod.rs
@@ -1,5 +1,3 @@
-//! Includes a comprehensive list of all constraint violations.
-
 use std::ops::Deref;
 
 /// Comprehensive list of all constraint violations.
@@ -10,27 +8,87 @@ use std::ops::Deref;
 /// Unfortunately, constraints cannot be automatically generated
 /// from the database definition (unlike entities), so the list
 /// must be maintained manually.
-#[derive(Debug, Default, Copy, Clone, PartialEq, Eq)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 #[must_use = "constraints do nothing unless you use them"]
 pub enum ConstraintViolation {
-    // TODO: Remove Self::Unknown.
-    #[default]
-    Unknown,
+    // Accounts table constraints.
+    AccountsNonEmptyName,
+    AccountsNonEmptyEmail,
+    AccountsNonEmptyPassword,
+    AccountsUpdatedAfterCreated,
+    AccountsDeletedAfterCreated,
+    AccountsDeletedAfterUpdated,
+
+    // Account Sessions table constraints.
+    SessionsRegionAlnum,
+    SessionsExpiredAfterIssued,
+    SessionsDeletedAfterIssued,
+
+    // Account Permissions table constraints.
+    PermissionsUpdatedAfterCreated,
+    PermissionsDeletedAfterCreated,
+    PermissionsDeletedAfterUpdated,
+
+    // Account Actions table constraints.
+    ActionsExpiredAfterIssued,
+    ActionsUsedAfterIssued,
 }
 
 impl ConstraintViolation {
     /// Returns a new [`ConstraintViolation`].
     pub fn new(constraint: &str) -> Option<Self> {
-        match constraint {
-            _ => None,
-        }
+        Some(match constraint {
+            // Accounts table constraints.
+            "accounts_non_empty_display_name" => Self::AccountsNonEmptyName,
+            "accounts_non_empty_email_address" => Self::AccountsNonEmptyEmail,
+            "accounts_non_empty_password_hash" => Self::AccountsNonEmptyPassword,
+            "accounts_updated_after_created" => Self::AccountsUpdatedAfterCreated,
+            "accounts_deleted_after_created" => Self::AccountsDeletedAfterCreated,
+            "accounts_deleted_after_updated" => Self::AccountsDeletedAfterUpdated,
+
+            // Account Sessions table constraints.
+            "account_sessions_region_alphanumeric" => Self::SessionsRegionAlnum,
+            "account_sessions_expired_after_issued" => Self::SessionsExpiredAfterIssued,
+            "account_sessions_deleted_after_issued" => Self::SessionsDeletedAfterIssued,
+
+            // Account Permissions table constraints.
+            "account_permissions_updated_after_created" => Self::PermissionsUpdatedAfterCreated,
+            "account_permissions_deleted_after_created" => Self::PermissionsDeletedAfterCreated,
+            "account_permissions_deleted_after_updated" => Self::PermissionsDeletedAfterUpdated,
+
+            // Account Actions table constraints.
+            "account_actions_expired_after_issued" => Self::ActionsExpiredAfterIssued,
+            "account_actions_used_after_issued" => Self::ActionsUsedAfterIssued,
+
+            _ => return None,
+        })
     }
 
     /// Returns the constraint name.
     #[must_use]
     pub fn as_str(&self) -> &str {
         match self {
-            ConstraintViolation::Unknown => "unknown",
+            // Accounts table constraints.
+            Self::AccountsNonEmptyName => "accounts_non_empty_display_name",
+            Self::AccountsNonEmptyEmail => "accounts_non_empty_email_address",
+            Self::AccountsNonEmptyPassword => "accounts_non_empty_password_hash",
+            Self::AccountsUpdatedAfterCreated => "accounts_updated_after_created",
+            Self::AccountsDeletedAfterCreated => "accounts_deleted_after_created",
+            Self::AccountsDeletedAfterUpdated => "accounts_deleted_after_updated",
+
+            // Account Sessions table constraints.
+            Self::SessionsRegionAlnum => "account_sessions_region_alphanumeric",
+            Self::SessionsExpiredAfterIssued => "account_sessions_expired_after_issued",
+            Self::SessionsDeletedAfterIssued => "account_sessions_deleted_after_issued",
+
+            // Account Permissions table constraints.
+            Self::PermissionsUpdatedAfterCreated => "account_permissions_updated_after_created",
+            Self::PermissionsDeletedAfterCreated => "account_permissions_deleted_after_created",
+            Self::PermissionsDeletedAfterUpdated => "account_permissions_deleted_after_updated",
+
+            // Account Actions table constraints.
+            Self::ActionsExpiredAfterIssued => "account_actions_expired_after_issued",
+            Self::ActionsUsedAfterIssued => "account_actions_used_after_issued",
         }
     }
 }
@@ -50,11 +108,18 @@ mod test {
 
     #[test]
     fn parse_constraint_violation() {
-        let _ = ConstraintViolation::new("unknown_constraint");
+        assert_eq!(
+            ConstraintViolation::new("accounts_non_empty_display_name"),
+            Some(ConstraintViolation::AccountsNonEmptyName)
+        );
+        assert_eq!(ConstraintViolation::new("unknown_constraint"), None);
     }
 
     #[test]
     fn stringify_constraint_violation() {
-        let _ = ConstraintViolation::Unknown.as_str();
+        assert_eq!(
+            ConstraintViolation::AccountsNonEmptyName.as_str(),
+            "accounts_non_empty_display_name"
+        );
     }
 }
