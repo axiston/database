432b97a778c00703f47ba007b5fc20d8cd4efa22
Oleh Martsokha <o.martsokha@gmail.com>
2024-12-14 14:42:58 +0100

    fix(ci): multiple crates publish

--- crates/migrate/config/embedded_query.rs
+++ crates/migrate/config/embedded_query.rs
@@ -3,4 +3,4 @@
 use diesel_migrations::{embed_migrations, EmbeddedMigrations};
 
 /// Migration source that embeds migrations into the final binary.
-pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("./../../migrations");
+pub const MIGRATIONS: EmbeddedMigrations = embed_migrations!("../../../migrations");
